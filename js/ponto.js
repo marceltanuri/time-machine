b81a0fb443952bd96cee63722b3a2e38ba029ada
Marcel Tanuri <marcel.tanuri@gmail.com>
2021-06-09 09:53:11 -0300

    improve log messages

--- js/ponto.js
+++ js/ponto.js
@@ -2,7 +2,7 @@ const api = require('./api');
 const myArgs = process.argv.slice(2);
 let datetime = myArgs[0];
 if(datetime==null){
-    console.error('Date and time must be provided.');
+    console.error('Date and time must be provided. Expected format: yyyy-MM-ddTHH:mm:ss');
     return;
 }
 
@@ -11,7 +11,7 @@ let isDate = function(date) {
 }
 
 if(!isDate(datetime)){
-    console.error('Not a valid date.');
+    console.error('Not a valid date. Expected format: yyyy-MM-ddTHH:mm:ss');
     return;
 }
 
